b21ae1b14211687fb53565711d64bc3b6b9e02fb
alexmeia <alexmeia@gmail.com>
2017-07-02 00:19:57 +0200

    Added method to retrieve last saved graph

--- src/main/java/it/phoops/mint/otp/dao/GraphBuilderDao.java
+++ src/main/java/it/phoops/mint/otp/dao/GraphBuilderDao.java
@@ -2,9 +2,9 @@ package it.phoops.mint.otp.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.Date;
 
 import it.phoops.mint.otp.model.GraphProperties;
 
@@ -18,13 +18,13 @@ public class GraphBuilderDao {
 	
 	public void saveGraphProperties(GraphProperties gp) throws Exception {
 		
-		String sql = " insert into otp_graph_builder.saved_graph "
+		String sql = " insert into graph_builder.saved_graph "
 				   + " (vertices, edges, has_streets, has_transit, creation_date, transit_modes, has_direct_transfers, agencies_size)"
 				   + " values (?, ?, ?, ?, ?, ?, ?, ?)";
 		
 		PreparedStatement ps = null;
 		
-		int i = 0;
+		int i = 1;
 	
 		ps = connection.prepareStatement(sql);
 		ps.setInt(i++, gp.getVertices());
@@ -37,7 +37,34 @@ public class GraphBuilderDao {
 		ps.setInt(i++, gp.getAgencies());
 		
 		ps.executeUpdate();
+		connection.commit();
+		
 		ps.close();
+		
+	}
+	
+	public GraphProperties getLastSavedGraph() throws Exception {
+		
+		GraphProperties lastSavedGraph = null;
+		
+		String sql = "SELECT vertices, edges, has_streets, has_transit, creation_date, transit_modes, has_direct_transfers, agencies_size "
+				   + "from graph_builder.saved_graph "
+				   + "order by creation_date desc limit 1";
+		
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		
+		while (rs.next()) {
+			lastSavedGraph = new GraphProperties();
+			lastSavedGraph.setAgencies(rs.getInt("agencies_size"));
+			lastSavedGraph.setVertices(rs.getInt("vertices"));
+			lastSavedGraph.setEdges(rs.getInt("edges"));
+			lastSavedGraph.setTransitModes(rs.getString("transit_modes"));
+		}
+		
+		ps.close();
+		
+		return lastSavedGraph;
 	}
 
 }
